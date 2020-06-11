a6e2c47e44d4be7fa89f774d876a3fe75eab00ca
nalini21 <naliniranjan21@gmail.com>
2020-06-11 13:27:02 +0530

    added Rive's Guss animation

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,9 @@
 import 'package:Number_trivia/loading.dart';
 import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'dart:math';
+import 'dart:async';
 
 class Home extends StatefulWidget {
   @override
@@ -10,56 +12,117 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int number;
+  String status;
+  @override
+  void initState() {
+    super.initState();
+    status = "idle";
+  }
+
+  bool isopen = true;
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context,
         width: 1080, height: 2340, allowFontScaling: false);
     return Scaffold(
-      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.black,
+      resizeToAvoidBottomInset: false,
       body: Container(
         alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
+          //mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(
-              height: 250.h,
-              width: 500.w,
-              child: TextField(
-                onChanged: (text) {
-                  number = int.parse(text);
-                },
-                decoration: InputDecoration(
-                    hintText: 'Enter an Integer',
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Colors.green[400], width: 5.0),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Colors.greenAccent, width: 5.0),
+            Center(
+              child: Container(
+                child: SizedBox(
+                    height: 1500.h,
+                    child: FlareActor(
+                      "assets/Guss1.flr",
+                      animation: "$status",
                     )),
               ),
             ),
-            RaisedButton(
-                color: Colors.blue,
-                child: Text('Enter'),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Loading(number,0)));
-                }),
-            SizedBox(height: 50.h),
-            RaisedButton(
-                color: Colors.greenAccent,
-                child: Text('Random Trivia'),
-                onPressed: () {
-                  Random random = new Random();
-                  int randomNumber = random.nextInt(2000);
-                  Random random2 = new Random();
-                  int randomNumber2 = random2.nextInt(randomNumber);
+            Center(
+              child: Container(
+                //padding: EdgeInsets.fromLTRB(150.w, 800.h, 150.w, 0),
+                child: SizedBox(
+                  height: 250.h,
+                  width: 500.w,
+                  child: TextField(
+                    onChanged: (text) {
+                      isopen = false;
+                      number = int.parse(text);
+                      if (!isopen) {
+                        setState(() {
+                          status = "cover_eyes_in";
+                          //await Future.delayed(const Duration(seconds: 5));
+                          //status = "cover_eyes_out";
+                        });
+                      }
+                    },
+                    decoration: InputDecoration(
+                        hintText: 'Enter an Integer',
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold,fontSize: 10.ssp,color: Colors.blue),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.green[400], width: 5.0.w),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent, width: 5.0.w),
+                        )),
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Container(
+                padding: EdgeInsets.only(top: 190.h),
+                child: RaisedButton(
+                    color: Colors.blue,
+                    child: Text('Enter'),
+                    onPressed: () {
+                      setState(() {
+                        status = "cover_eyes_out";
+                        //Future.delayed(const Duration(seconds: 2));
+                      });
+                      Timer(Duration(seconds: 1), () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Loading(number, 0)));
+                      });
+                     
+                    }),
+              ),
+            ),
+            Center(
+              child: Container(
+                padding: EdgeInsets.only(top: 400.h),
+                child: RaisedButton(
+                    color: Colors.greenAccent,
+                    child: Text('Random Trivia'),
+                    onPressed: () {
+                      
+                      setState(() {
+                        status = "success";
+                        //Future.delayed(const Duration(seconds: 2));
+                      });
+                      Random random = new Random();
+                      int randomNumber = random.nextInt(2000);
+                      Random random2 = new Random();
+                      int randomNumber2 = random2.nextInt(randomNumber);
 
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Loading(randomNumber2+1,1)));
-                }),
+                      Timer(Duration(seconds: 1), () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    Loading(randomNumber2 + 1, 1)));
+                      });
+                    }),
+              ),
+            ),
           ],
         ),
       ),
--- lib/output_page.dart
+++ lib/output_page.dart
@@ -1,3 +1,4 @@
+import 'package:Number_trivia/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
